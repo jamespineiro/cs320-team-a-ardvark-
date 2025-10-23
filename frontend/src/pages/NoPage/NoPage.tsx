65111ebf3cb96e0128c0225f2b29135ee6fb6499
LeoneVi <121581270+LeoneVi@users.noreply.github.com>
2025-10-23 18:03:06 -0400

    SCRUM-1 Pages now load

--- frontend/src/pages/NoPage/NoPage.tsx
+++ frontend/src/pages/NoPage/NoPage.tsx
@@ -1,9 +1,8 @@
-export function NoPage() {
+export default function NoPage() {
     return (
         <div>
-            <h2>404: Page Not Found</h2>
+            <h1>404 — Not Found</h1>
+            <p>The page does not exist.</p>
         </div>
     );
 }
-
-export default NoPage;
