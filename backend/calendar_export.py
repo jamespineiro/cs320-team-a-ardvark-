0a88fe660b549ad41f1d9cd899a96822e371e88b
Keerthi C <chebroluk@gmail.com>
2025-11-05 16:42:18 -0500

    created backend folder - need to test basic ics

--- backend/calendar_export.py
+++ backend/calendar_export.py
@@ -1,5 +1,6 @@
-from ics import Calendar, Event
+from backend.calendar_export import Calendar, Event
 
+"""
 def convert_deadlines(deadlines, filename="deadlines.ics"):
     c = Calendar()
     for d in deadlines:
@@ -12,3 +13,16 @@ def convert_deadlines(deadlines, filename="deadlines.ics"):
         my_file.writelines(c.serialize_iter())
     # and it's done !
     return filename
+"""
+
+c = Calendar()
+e = Event()
+e.name = "My cool event"
+e.begin = '2014-01-01 00:00:00'
+c.events.add(e)
+c.events
+# [<Event 'My cool event' begin:2014-01-01 00:00:00 end:2014-01-01 00:00:01>]
+# with open('my.ics', 'w') as my_file:
+#     my_file.writelines(c.serialize_iter())
+print(c.serialize_iter())
+# and it's done !
