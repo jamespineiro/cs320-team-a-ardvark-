ccaa31f24e7bd239604b59946e93b30782dc96ef
LeoneVi <121581270+LeoneVi@users.noreply.github.com>
2025-11-05 13:28:26 -0500

    7 homepage created, calendar icon added

---
+++ frontend/src/pages/Home/Home.tsx
@@ -0,0 +1,9 @@
+import NavigationBar from "./NavigationBar/NavigationBar.tsx";
+
+const Home: React.FC = () => {
+    return (
+        <NavigationBar />
+    )
+}
+
+export default Home;
---
+++ frontend/src/pages/Home/NavigationBar/NavigationBar.tsx
@@ -0,0 +1,10 @@
+import CalendarIcon from "./icons/CalendarIcon.tsx"
+import React from "react";
+
+const NavigationBar: React.FC = () => {
+    return (
+        <CalendarIcon></CalendarIcon>
+    )
+}
+
+export default NavigationBar;
