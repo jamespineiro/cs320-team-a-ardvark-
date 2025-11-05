703d001abd8eb59657a0a812b0ebc828efba0dbd
LeoneVi <121581270+LeoneVi@users.noreply.github.com>
2025-11-04 19:14:33 -0500

    15 Loading screen works

--- frontend/src/components/index.ts
+++ frontend/src/components/index.ts
@@ -1,2 +1,3 @@
 export { default as Button } from './Shared/Button/Button';
 export { default as Input } from './Shared/Input/Input';
+export { default as Loading } from './Loading/Loading';
--- frontend/src/pages/Signup/Signup.css.ts
+++ frontend/src/pages/Signup/Signup.css.ts
@@ -84,3 +84,15 @@ export const loginLink = style({
         textDecoration: "underline",
     },
 });
+
+export const loaderContainer = style({
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    height: "100vh",
+    width: "100vw",
+    position: "fixed",
+    top: 0,
+    left: 0,
+    zIndex: 9999,
+});
