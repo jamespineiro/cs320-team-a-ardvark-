72e2382da0760794b137353dbcf841eb88b3ed40
Keerthi C <chebroluk@gmail.com>
2025-11-12 16:43:47 -0500

    removed comments

--- backend/calendar_export.py
+++ backend/calendar_export.py
@@ -8,25 +8,9 @@ def convert_deadlines(deadlines, filename="deadlines.ics"):
         event.name = f"{d['course']} - {d['assignment']}"
         event.begin = d['due_date']
         c.events.add(event)
-    # [<Event 'My cool event' begin:2014-01-01 00:00:00 end:2014-01-01 00:00:01>]
     with open(filename, 'w') as my_file:
         my_file.writelines(c.serialize_iter())
-    # and it's done !
     return filename
-
-"""
-c = Calendar()
-e = Event()
-e.name = "My cool event"
-e.begin = '2014-01-01 00:00:00'
-c.events.add(e)
-c.events
-# [<Event 'My cool event' begin:2014-01-01 00:00:00 end:2014-01-01 00:00:01>]
-# with open('my.ics', 'w') as my_file:
-#     my_file.writelines(c.serialize_iter())
-print(c.serialize_iter())
-# and it's done !
-"""
 
 deadlines = [
     {
