23849dadc2400b06be45d0de297d099a1f4f9855
LeoneVi <121581270+LeoneVi@users.noreply.github.com>
2025-11-29 21:35:16 -0500

    deleted canvas calendar page

--- frontend/src/pages/index.ts
+++
@@ -1,6 +0,0 @@
-export { default as Signup } from './Signup/Signup';
-export { default as Login } from './Login/Login';
-export { default as Home } from './Home/Home';
-export { default as NoPage } from './NoPage/NoPage';
-export { default as CanvasCalendar } from './CanvasCalendar';
-export { default as Launch } from './Launch/Launch';
