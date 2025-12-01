c21caa2c4b0fb888c61c5bfbf913dda72b9333fb
Keerthi C <chebroluk@gmail.com>
2025-12-01 16:52:29 -0500

    reformatted GradescopeDeadlines.py for unit test

---
+++ backend/tests/gradescope_deadlines_test.py
@@ -0,0 +1,18 @@
+import pytest
+from unittest.mock import Mock
+from backend.python.GradescopeDeadlines import login, scrape_courses_and_assignments, BASE
+
+def test_correct_login():
+    return
+
+def test_login_with_no_token():
+    return
+
+def test_login_with_wrong_credentials():
+    return
+
+def test_scrape_zero_courses():
+    return
+
+def test_scrape_one_course_and_assignment():
+    return
