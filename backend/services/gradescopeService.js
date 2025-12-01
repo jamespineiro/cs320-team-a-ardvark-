a8ad7b85982215173025365b9c059cc04cf821c8
LeoneVi <121581270+LeoneVi@users.noreply.github.com>
2025-12-01 09:49:49 -0500

    fixed spacing issue

--- backend/services/gradescopeService.js
+++ backend/services/gradescopeService.js
@@ -1,6 +1,5 @@
 const { spawn } = require("child_process");
 
-
 function runGradescope(email, password) {
     return new Promise((resolve, reject) => {
         const python = spawn("python3", ["./python/GradescopeDeadlines.py", email, password]);
